4ab5042b163c55407f24bf72bca50b9ef295bf13
waltson <waltson.silva@hotmail.com>
2018-08-14 10:06:18 -0300

    implementando autorização

--- spring-boot-ionic-backend/src/main/java/mcagile/com/config/SecurityConfig.java
+++ spring-boot-ionic-backend/src/main/java/mcagile/com/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import mcagile.com.security.JWTAuthenticationFilter;
+import mcagile.com.security.JWTAuthorizationFilter;
 import mcagile.com.security.JWTUtil;
 
 @Configuration
@@ -49,6 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil,userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
