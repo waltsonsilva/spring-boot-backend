057ac8dc112235e5e5f01973a153b383e879490f
waltson <waltson.silva@hotmail.com>
2018-08-15 14:52:44 -0300

    Expondo o header Authorization (problema de Cors)

--- spring-boot-ionic-backend/src/main/java/mcagile/com/resource/AuthResource.java
+++ spring-boot-ionic-backend/src/main/java/mcagile/com/resource/AuthResource.java
@@ -20,17 +20,18 @@ import mcagile.com.services.UserService;
 @RequestMapping(value = "/auth")
 public class AuthResource {
 
- @Autowired
- private JWTUtil jwtUtil ;	
- 
- @Autowired
- private AuthService authService;
-	
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@Autowired
+	private AuthService authService;
+
 	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
+		response.addHeader("access-control-expose-headers", "Authorization");
 		return ResponseEntity.noContent().build();
 	}
 
