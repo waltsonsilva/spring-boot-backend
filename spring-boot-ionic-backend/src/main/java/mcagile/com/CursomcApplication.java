92f5c814939024486372afbe26200c1b1dbd8d89
waltson <waltson.silva@hotmail.com>
2018-08-15 09:47:24 -0300

    salvando arquivo no s3

--- spring-boot-ionic-backend/src/main/java/mcagile/com/CursomcApplication.java
+++ spring-boot-ionic-backend/src/main/java/mcagile/com/CursomcApplication.java
@@ -1,11 +1,17 @@
 package mcagile.com;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import mcagile.com.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private S3Service s3service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -14,6 +20,8 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		s3service.uploadFile("/Users/virtualvision0114/Desktop/teste.png");
+		
 	}
 
 }
